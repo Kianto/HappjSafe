ef709cc604bd173c62e1605637d2d4601fa60c50
Bui Van Xia <buivanxia10@gmail.com>
2020-04-25 20:38:58 +0700

    add and change some thing

--- lib/views/user_scanner.dart
+++ lib/views/user_scanner.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
-
+import 'package:happjsafe/models/journey.dart';
+import 'package:happjsafe/service/services.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 class UserScannerPage extends StatefulWidget {
+  UserScannerPage({Key key, @required this.userId}) : super(key: key);
+
+  final String userId;
+
   @override
   _UserScannerPageState createState() => _UserScannerPageState();
 }
 
 class _UserScannerPageState extends State<UserScannerPage> {
-  String _provider ="Vietnam Airline";
-  String _from ="Thủ dầu một, Bình Dương, Việt Nam ";
-  String _to ="San Franssico, United State, USA";
-  String _datefrom="20 Apr 2020, 20:30 PM";
-  String _dateto="30 Apr 2020, 24:00 AM";
+  String _provider;
+  String _from;
+  String _to;
+  String _datefrom;
+  String _dateto;
+
+  String scanResult = "...";
+  Journey route;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _provider = "Vietnam Airline";
+      _from = "Thủ dầu một, Bình Dương, Việt Nam ";
+      _to = "San Franssico, United State, USA";
+      _datefrom = "20 Apr 2020, 20:30 PM";
+      _dateto = "30 Apr 2020, 24:00 AM";
+    });
+    _scan();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,143 +45,192 @@ class _UserScannerPageState extends State<UserScannerPage> {
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-           Stack(
-             children: <Widget>[
-               Container(
-                 alignment: Alignment.bottomCenter,
-                // margin: EdgeInsets.all(10),
-                padding: EdgeInsets.fromLTRB(0, 70, 30, 0),
-                 child: Container(
-                   width: 200,
-                   height: 180,
-                   decoration: BoxDecoration(
-                     color: Colors.white,
-                     borderRadius: BorderRadius.circular(30)
-                   ),
-                 ),
-               ),
-               Container(
-                 alignment: Alignment.topCenter,
-                 child: Image.asset("assets/images/like.png")
-                 ),
-             ],
-           ),
-           SizedBox(height: 20,),
-           Text("Done!",style: TextStyle(fontSize: 40,color: Colors.white, fontWeight: FontWeight.bold),),
-           SizedBox(height: 20,),
-           Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(width:1,color:Colors.white)
-            ),
-           ),
-           SizedBox(height: 20,),
-           Container(
-             padding: EdgeInsets.all(20),
-             child: Column(
-               children: <Widget>[
-                Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                   "Provider: ",
-                   style: TextStyle(fontSize: 20,color: Colors.white),
-                    ),
-                SizedBox(width: 5),
-                Text(
-                  _provider,
-                   style: TextStyle(
-                    fontSize: 20, fontWeight: FontWeight.bold,color: Colors.white),
-                      ),
-                     ],
-                    ),
-                Row(
+              SizedBox(height: 20),
+              Stack(
+                children: <Widget>[
+                  // Container(
+                  //   alignment: Alignment.bottomCenter,
+                  //   // margin: EdgeInsets.all(10),
+                  //   padding: EdgeInsets.fromLTRB(0, 70, 30, 0),
+                  //   child: Container(
+                  //     width: 200,
+                  //     height: 180,
+                  //     decoration: BoxDecoration(
+                  //         color: Colors.white,
+                  //         borderRadius: BorderRadius.circular(30)),
+                  //   ),
+                  // ),
+                  Container(
+                      height: 100,
+                      width: 100,
+                      //alignment: Alignment.topCenter,
+                      child: Image.asset("assets/images/done.gif", fit: BoxFit.fill,)),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Done!",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                decoration: BoxDecoration(
+                  
+                    color: Colors.white,
+                    border: Border.all(width: 1, color: Colors.white)),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
                   children: <Widget>[
-                    Text(
-                    "From: ",
-                    style: TextStyle(fontSize: 20,color: Colors.white),
-                      ),
-                SizedBox(width: 5),
-                Expanded(
-                  child: Text(
-                    _from,
-                     style: TextStyle(
-                         fontSize: 18,color: Colors.white),
-                              )),
-                        ],
-                      ),
-                SizedBox(height: 10),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "To: ",
-                      style: TextStyle(fontSize: 20,color: Colors.white),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Provider: ",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
                         ),
-                  SizedBox(width: 5),
-                  Expanded(
-                    child: Text(
-                    _to,
-                      style: TextStyle(
-                      fontSize: 18, color: Colors.white),
-                              )),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        children: <Widget>[
-                          Text(
-                            "Date: ",
-                            style: TextStyle(fontSize: 20,color: Colors.white),
-                          ),
-                          SizedBox(width: 5),
-                          Column(
-                            children: <Widget>[
-                              Text(
-                                _datefrom,
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white),
-                              ),
-                              Text(
-                                "- - - - - To - - - - -",
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.yellowAccent),
-                              ),
-                              Text(
-                                _dateto,
-                                style: TextStyle(
-                                    fontSize: 18,
-                                   color: Colors.white),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-               ],
-             ),
-           ),
-           Container(
-             margin: EdgeInsets.all(20),
+                        SizedBox(width: 5),
+                        Text(
+                          _provider,
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "From: ",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                        SizedBox(width: 5),
+                        Expanded(
+                            child: Text(
+                          _from,
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        )),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "To: ",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                        SizedBox(width: 5),
+                        Expanded(
+                            child: Text(
+                          _to,
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        )),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          "Date: ",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                        SizedBox(width: 5),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              _datefrom,
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                            Text(
+                              "- - - - - To - - - - -",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.yellowAccent),
+                            ),
+                            Text(
+                              _dateto,
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
                 width: double.infinity,
                 height: 50,
-                  child: RaisedButton(
-                  child:Text("Continue Scanning",style: TextStyle(fontSize: 20,color: Colors.white, fontWeight: FontWeight.bold)) ,
-                   onPressed:_continue ,
-                   color: Colors.blue[600],
-                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))
-                        ),
-                     ),
-                  ),
+                child: RaisedButton(
+                  child: Text("Continue Scanning",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold)),
+                  onPressed: _continue,
+                  color: Colors.blue[600],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-  void  _continue(){
 
+  void _continue() => _scan();
+
+  Future _scan() async {
+    String barcode = await scanner.scan();
+    print(barcode);
+    setState(() {
+      scanResult = barcode;
+    });
+    var data = ServiceService.get_service_info(barcode);
+
+    data.then((value){
+      setState(() {
+        _provider = value['provider'];
+        _from = value['fromPlace'];
+        _to = value['toPlace'];
+        _datefrom = value['fromTime'];
+        _dateto = value['toTime'];
+      });
+    });
+
+    data.then((value) {
+      route = new Journey(
+        serviceId: barcode,
+        fromPlace: value['fromPlace'],
+        toPlace: value['toPlace'],
+        fromTime: value['fromTime'].toDate(),
+        toTime: value['toTime'].toDate(),
+        isGood: value['isGood'],
+      );
+      UserService.update_user_history(widget.userId, route);
+      ServiceService.update_passenger(widget.userId, barcode);
+    });
+    //route = new Journey.fromJson(ServiceService.get_service_info(barcode));
   }
 }
